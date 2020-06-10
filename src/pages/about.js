af49cae057ccd2fd29b35400d32cac4eab5c0a2b
Brittney <brittney.postma@hotmail.com>
2020-06-10 18:31:16 -0400

    init

---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>
+        I'm baby tofu cray migas authentic mumblecore. Fingerstache kombucha
+        mumblecore poke asymmetrical ugh squid pug crucifix. Photo booth VHS
+        PBR&B vape tote bag. Butcher occupy heirloom, flannel hell of pitchfork
+        mlkshk marfa cardigan. Beard snackwave mixtape artisan lo-fi semiotics.
+        Pinterest asymmetrical glossier, farm-to-table try-hard activated
+        charcoal echo park.
+      </p>
+    </Layout>
+  )
+}
+
+export default About
